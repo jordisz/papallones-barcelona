3fc5e8e0ac7ecfe755fac11f7701d14aed1fcc15
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2021-12-20 12:51:12 +0100

    Add ParcSelector and ButterflyCard to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,49 @@
 <template>
-  <Tutorial />
+  <div>
+    <div class="filters-bar">
+      <ParcSelector v-on:parc-seleccionat="filtraParc" />
+    </div>
+    <div class="cards-container">
+      <ButterflyCard v-for="(especie, index) in filteredData.especies" :key="index" :especie="especie" />
+    </div>
+  </div>
 </template>
 
 <script>
+import ParcSelector from '@/components/ParcSelector'
+import ButterflyCard from '@/components/ButterflyCard.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    ParcSelector,
+    ButterflyCard
+  },
+  data () {
+    return {
+      filteredData: []
+    }
+  },
+  created () {
+    this.$store.dispatch('getData')
+  },
+  methods: {
+    filtraParc (parc) {
+      this.filteredData = this.$store.getters.getParcData(parc)
+    }
+  }
 }
 </script>
+
+<style>
+.filters-bar {
+  width: 100vw;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
