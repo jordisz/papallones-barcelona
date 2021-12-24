23c3069b93d68ef456cd7de3501c6a2ad4e481cc
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2021-12-24 14:03:31 +0100

    Create AppSelector component

---
+++ components/AppSelector.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <select v-model="selected" @change="emitSelectedItem">
+      <option v-for="(item, index) in list" :key="index" :value="item">
+        {{ item }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSelector',
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      selected: this.list[0]
+    }
+  },
+  methods: {
+    emitSelectedItem () {
+      this.$emit('item-selected', this.selected)
+    }
+  }
+}
+</script>
