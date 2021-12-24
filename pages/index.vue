08308ebf121351eafe5aa57eb537faa9a6d570d3
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2021-12-24 14:08:11 +0100

    Add total computing, sorting and filtering by year

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,94 @@
 <template>
   <div>
     <div class="filters-bar">
-      <ParcSelector v-on:parc-seleccionat="filtraParc" />
+      <AppSelector :list="parcNamesArray" v-on:item-selected="filtraParc" />
+      <AppSelector :list="parcYearsArray" v-on:item-selected="filtraData" />
     </div>
-    <div class="cards-container">
-      <ButterflyCard v-for="(especie, index) in filteredData.especies" :key="index" :especie="especie" />
+    <div class="summary">
+      {{ parcName }} ({{ selectedYear }}) - {{ filteredByYear.length }} espècies
     </div>
+    <transition-group name="flip-cards" tag="div" class="cards-container">
+      <ButterflyCard v-for="especie in filteredByYear" :key="especie[0]" :especie="especie" />
+    </transition-group>
   </div>
 </template>
 
 <script>
-import ParcSelector from '@/components/ParcSelector'
+import Parcs from '@/store/parcs.json'
+import AppSelector from '@/components/AppSelector'
 import ButterflyCard from '@/components/ButterflyCard.vue'
 export default {
   name: 'IndexPage',
   components: {
-    ParcSelector,
+    AppSelector,
     ButterflyCard
   },
   data () {
     return {
-      filteredData: []
+      Parcs,
+      parcName: '',
+      orderedData: [],
+      selectedYear: 'TOTAL HISTORIC'
+    }
+  },
+  computed: {
+    parcNamesArray () {
+      let arr = []
+      this.Parcs.forEach((parc) => {
+        arr.push(parc.nom)
+      })
+      arr = ['TOTS ELS PARCS', ...arr]
+      return arr
+    },
+    parcYearsArray () {
+      let yearsArray = []
+      this.orderedData.forEach((especie) => {
+        especie[1].forEach((observacio) => {
+          const ddmmyyyy = observacio.fecha.split('-')
+          const year = ddmmyyyy[2]
+          if (!yearsArray.includes(year)) {
+            yearsArray.push(year)
+          }
+        })
+      })
+      yearsArray.sort()
+      yearsArray = ['TOTAL HISTORIC', ...yearsArray]
+      return yearsArray
+    },
+    filteredByYear () {
+      if (this.selectedYear === 'TOTAL HISTORIC') {
+        return this.orderedData
+      }
+      const filteredArray = []
+      this.orderedData.forEach((especie) => {
+        const tempEspecie = []
+        tempEspecie[0] = especie[0]
+        tempEspecie[1] = especie[1].filter(d => d.fecha.includes(this.selectedYear))
+        if (tempEspecie[1].length > 0) {
+          filteredArray.push(tempEspecie)
+        }
+      })
+      filteredArray.sort((a, b) => b[1].length - a[1].length)
+      return filteredArray
     }
   },
   created () {
-    this.$store.dispatch('getData')
+    this.$store.dispatch('setInitialData')
   },
   methods: {
     filtraParc (parc) {
-      this.filteredData = this.$store.getters.getParcData(parc)
+      this.parcName = parc
+      if (parc === 'TOTS ELS PARCS') {
+        const receivedTotalData = this.$store.getters.getTotalData
+        this.orderedData = receivedTotalData.sort((a, b) => b[1].length - a[1].length)
+      } else {
+        const filteredData = this.$store.getters.getParcData(parc).observacions
+        this.orderedData = filteredData.sort((a, b) => b[1].length - a[1].length)
+      }
+    },
+    filtraData (year) {
+      this.selectedYear = year
+      return year
     }
   }
 }
@@ -42,8 +102,28 @@ export default {
   align-items: center;
 }
 
+.summary {
+  text-align: center;
+}
+
 .cards-container {
   display: flex;
   flex-wrap: wrap;
+  gap: 1rem;
+  margin-top: 1rem;
+}
+
+.flip-cards-enter-active,
+.flip-cards-leave-active {
+  transition: all 1s;
+}
+
+.flip-cards-enter,
+.flip-cards-leave-to {
+  opacity: 0;
+}
+
+.flip-cards-move {
+  transition: transform 1s;
 }
 </style>
