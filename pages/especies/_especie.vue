a256928f4674dd76148047ec925ccb7015654074
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-02 20:55:10 +0100

    Create especie dynamic page

---
+++ pages/especies/_especie.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p><em>{{ info.familia }}</em></p>
+    <h2>{{ info.nomCat }}</h2>
+    <h3>{{ info.nomCientific }}</h3>
+    <ul>
+      <li>Cast: {{ info.nomCast }}</li>
+      <li>Eng: {{ info.nomEng }}</li>
+    </ul>
+    {{ id }}
+  </div>
+</template>
+
+<script>
+import especiesInfo from '@/store/especies.json'
+export default {
+  data () {
+    return {
+      id: this.$route.params.especie,
+      especiesInfo
+    }
+  },
+  computed: {
+    info () {
+      const index = this.especiesInfo.findIndex(especie => especie.id === parseInt(this.id))
+      return this.especiesInfo[index]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
