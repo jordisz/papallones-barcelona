04ae511acbab6b3ba6c1821dc4f3934d75ff833c
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-05 18:03:48 +0100

    Create SpeciesCard component

---
+++ components/SpeciesCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <img :src="require(`~/assets/butterfly-icons/${especie.imatge}`)" alt="" class="especie-thumbnail">
+    {{ especie.nomCat }}
+    <NuxtLink class="link-sp" :to="`/especies/${especie.id}`">
+      Veure fitxa
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    especie: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+    height: 4rem;
+    display: flex;
+    align-items: center;
+}
+.especie-thumbnail {
+  width: 4rem;
+}
+</style>
