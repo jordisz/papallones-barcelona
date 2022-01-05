ec470fc892042288ab53db2a9f0f305e816c16e3
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-05 13:01:40 +0100

    Create header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <p class="title">
+      Les papallones de Barcelona
+    </p>
+    <ul class="nav">
+      <li>
+        <NuxtLink to="#">
+          Espècies
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">
+          Recomptes parcs uBMS
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="#">
+          Preguntes freqüents
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  padding: 0 4vw;
+  text-transform: uppercase;
+  background-color: rgba(90, 169, 90, 0.3);
+}
+
+.nav {
+  display: flex;
+  list-style: none;
+}
+.nav li {
+  margin: 0;
+  padding: 0 1rem;
+}
+.nav li a {
+  text-decoration: none;
+}
+
+</style>
