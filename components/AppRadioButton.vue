98f217fa8ddb8b6313e0cb5444ddabf69683fdf2
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-07 18:03:39 +0100

    Create radio button and button group components

---
+++ components/AppRadioButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label :for="option">
+      {{ option }}
+    </label>
+    <input
+      :id="option"
+      type="radio"
+      name="radio-input"
+      :value="option"
+      :checked="checked === option"
+      @change="$parent.$emit('input', option)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppRadioButton',
+  props: {
+    option: {
+      required: true,
+      type: String
+    },
+    checked: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/AppRadioButtonGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="button-group">
+    <label :for="id"><slot></slot></label>
+    <AppRadioButton v-for="(option, index) in options" :id="id" :key="index" :checked="checked" :option="option" />
+  </div>
+</template>
+
+<script>
+import AppRadioButton from '~/components/AppRadioButton.vue'
+export default {
+  name: 'AppRadioButtonGroup',
+  components: { AppRadioButton },
+  props: {
+    options: {
+      required: true,
+      type: Array
+    },
+    checked: {
+      required: true,
+      type: String
+    },
+    id: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button-group {
+    margin: 0 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    gap: .4rem;
+}
+</style>
