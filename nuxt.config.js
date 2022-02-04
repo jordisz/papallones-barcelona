0a1737033148b5c03191f3037c95f6bdc43a8d39
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-02-04 16:32:15 +0100

    Change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ubms-data',
+    title: 'Les papallones de Barcelona',
     htmlAttrs: {
       lang: 'ca'
     },
