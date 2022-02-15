33b4eb47f57ac1af185496b6860cce98160831ad
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-02-15 18:03:05 +0100

    Improve configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/papallones-barcelona/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
