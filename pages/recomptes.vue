35051e0ba6a086954b8baf716c77b79031004699
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2025-05-11 17:38:37 +0200

    prettier

--- pages/recomptes.vue
+++ pages/recomptes.vue
@@ -4,154 +4,180 @@
       <p>Carregant dades...</p>
     </div>
     <div v-if="isDataReady" class="page-container">
-      <h1 class="titol">
-        Recomptes als parcs de Barcelona
-      </h1>
+      <h1 class="titol">Recomptes als parcs de Barcelona</h1>
       <div class="summary">
-        <div>{{ parcName }} ({{ selectedYear }}) - {{ countOnlyDeterminedSpecies }} espècies</div>
+        <div>
+          {{ parcName }} ({{ selectedYear }}) -
+          {{ countOnlyDeterminedSpecies }} espècies
+        </div>
       </div>
       <div class="filters-bar">
-        <AppSelector class="selector" id-name="filtraParc" label="Filtra per parc" :list="parcNamesArray" @item-selected="filtraParc" />
-        <AppSelector class="selector" id-name="filtraAny" label="Filtra per any" :list="parcYearsArray" @item-selected="setSelectedYear" />
-        <AppRadioButtonGroup id="ordre" v-model="orderCriterion" :checked="orderCriteria[0]" :options="orderCriteria">
+        <AppSelector
+          class="selector"
+          id-name="filtraParc"
+          label="Filtra per parc"
+          :list="parcNamesArray"
+          @item-selected="filtraParc"
+        />
+        <AppSelector
+          class="selector"
+          id-name="filtraAny"
+          label="Filtra per any"
+          :list="parcYearsArray"
+          @item-selected="setSelectedYear"
+        />
+        <AppRadioButtonGroup
+          id="ordre"
+          v-model="orderCriterion"
+          :checked="orderCriteria[0]"
+          :options="orderCriteria"
+        >
           Ordena per:
         </AppRadioButtonGroup>
       </div>
       <transition-group name="flip-cards" tag="div" class="cards-container">
-        <ButterflyCard v-for="especie in filteredByYear" :key="especie[0]" :especie="especie" />
+        <ButterflyCard
+          v-for="especie in filteredByYear"
+          :key="especie[0]"
+          :especie="especie"
+        />
       </transition-group>
     </div>
   </div>
 </template>
 
 <script>
-import Parcs from '~/store/parcs.json'
-import AppSelector from '~/components/AppSelector'
-import ButterflyCard from '~/components/ButterflyCard'
-import EspeciesInfo from '~/store/especies.json'
-import AppRadioButtonGroup from '~/components/AppRadioButtonGroup.vue'
+import Parcs from "~/store/parcs.json";
+import AppSelector from "~/components/AppSelector";
+import ButterflyCard from "~/components/ButterflyCard";
+import EspeciesInfo from "~/store/especies.json";
+import AppRadioButtonGroup from "~/components/AppRadioButtonGroup.vue";
 export default {
-  name: 'IndexPage',
+  name: "IndexPage",
   components: {
     AppSelector,
     AppRadioButtonGroup,
-    ButterflyCard
+    ButterflyCard,
   },
-  data () {
+  data() {
     return {
       /** Array with park names */
       Parcs,
       /** Current selected park name */
-      parcName: '',
+      parcName: "",
       /** Array with observations filtered by selected park (populated when data is ready and also by filtraParc method) */
       byParcData: [],
       /** Current selected year */
-      selectedYear: 'TOTAL HISTÒRIC',
+      selectedYear: "TOTAL HISTÒRIC",
       /** Attributes of the different species */
       EspeciesInfo,
-      orderCriteria: ['freqüència', 'abundància'],
-      orderCriterion: 'freqüència'
-    }
+      orderCriteria: ["freqüència", "abundància"],
+      orderCriterion: "freqüència",
+    };
   },
   computed: {
     /** Checks if data is fetched in store */
-    isDataReady () {
-      return this.$store.getters.fetchedStatus
+    isDataReady() {
+      return this.$store.getters.fetchedStatus;
     },
     /** Obtains a list with all the park names */
-    parcNamesArray () {
-      let arr = []
+    parcNamesArray() {
+      let arr = [];
       this.Parcs.forEach((parc) => {
-        arr.push(parc.nom)
-      })
-      arr = ['TOTS ELS PARCS', ...arr]
-      return arr
+        arr.push(parc.nom);
+      });
+      arr = ["TOTS ELS PARCS", ...arr];
+      return arr;
     },
     /** Obtains a list with all the years with observations in the current park */
-    parcYearsArray () {
-      let yearsArray = []
+    parcYearsArray() {
+      let yearsArray = [];
       this.byParcData.forEach((especie) => {
         especie[1].forEach((observacio) => {
-          const ddmmyyyy = observacio.fecha.split('-')
-          const year = ddmmyyyy[2]
+          const ddmmyyyy = observacio.fecha.split("-");
+          const year = ddmmyyyy[2];
           if (!yearsArray.includes(year)) {
-            yearsArray.push(year)
+            yearsArray.push(year);
           }
-        })
-      })
-      yearsArray.sort()
-      yearsArray = ['TOTAL HISTÒRIC', ...yearsArray]
-      return yearsArray
+        });
+      });
+      yearsArray.sort();
+      yearsArray = ["TOTAL HISTÒRIC", ...yearsArray];
+      return yearsArray;
     },
     /** Filters current park data by selected year */
-    filteredByYear () {
-      if (this.selectedYear === 'TOTAL HISTÒRIC') {
-        const sortedData = this.sortData(this.byParcData)
-        return sortedData
+    filteredByYear() {
+      if (this.selectedYear === "TOTAL HISTÒRIC") {
+        const sortedData = this.sortData(this.byParcData);
+        return sortedData;
       }
-      const filteredArray = []
+      const filteredArray = [];
       this.byParcData.forEach((especie) => {
-        const tempEspecie = []
-        tempEspecie[0] = especie[0]
-        tempEspecie[1] = especie[1].filter(d => d.fecha.includes(this.selectedYear))
+        const tempEspecie = [];
+        tempEspecie[0] = especie[0];
+        tempEspecie[1] = especie[1].filter((d) =>
+          d.fecha.includes(this.selectedYear)
+        );
         if (tempEspecie[1].length > 0) {
-          filteredArray.push(tempEspecie)
+          filteredArray.push(tempEspecie);
         }
-      })
-      const sortedData = this.sortData(filteredArray)
-      return sortedData
+      });
+      const sortedData = this.sortData(filteredArray);
+      return sortedData;
     },
     /** Returns count of species in park/year (ignoring undetermined observations) */
-    countOnlyDeterminedSpecies () {
-      const undetermined = this.EspeciesInfo
-        .filter(especie => especie.determinada === false)
-        .map(x => x.nomCientific)
-      return this.filteredByYear.filter(especie => !undetermined.includes(especie[0])).length
-    }
+    countOnlyDeterminedSpecies() {
+      const undetermined = this.EspeciesInfo.filter(
+        (especie) => especie.determinada === false
+      ).map((x) => x.nomCientific);
+      return this.filteredByYear.filter(
+        (especie) => !undetermined.includes(especie[0])
+      ).length;
+    },
   },
   watch: {
     isDataReady: function (oldval, dataReady) {
-      const receivedTotalData = this.$store.getters.getTotalData
-      this.byParcData = receivedTotalData
-    }
+      const receivedTotalData = this.$store.getters.getTotalData;
+      this.byParcData = receivedTotalData;
+    },
   },
-  created () {
-    this.$store.dispatch('setInitialData')
+  created() {
+    this.$store.dispatch("setInitialData");
   },
   methods: {
     /** Populates byParcData array with observations in the selected park */
-    filtraParc (parc) {
-      this.parcName = parc
-      if (parc === 'TOTS ELS PARCS') {
-        this.byParcData = this.$store.getters.getTotalData
+    filtraParc(parc) {
+      this.parcName = parc;
+      if (parc === "TOTS ELS PARCS") {
+        this.byParcData = this.$store.getters.getTotalData;
       } else {
-        this.byParcData = this.$store.getters.getParcData(parc).observacions
+        this.byParcData = this.$store.getters.getParcData(parc).observacions;
       }
     },
     /** Sets the value of selectedYear variable */
-    setSelectedYear (year) {
-      this.selectedYear = year
-      return year
+    setSelectedYear(year) {
+      this.selectedYear = year;
+      return year;
     },
-    sortData (data) {
+    sortData(data) {
       /** Get total number of exemplars of each species, save it in each species array[2]  */
       data.forEach((especie) => {
-        especie[2] = especie[1].reduce((prev, curr) => prev + curr.t, 0)
-      })
-      if (this.orderCriterion === 'abundància') {
-        return data.sort((a, b) => b[2] - a[2])
+        especie[2] = especie[1].reduce((prev, curr) => prev + curr.t, 0);
+      });
+      if (this.orderCriterion === "abundància") {
+        return data.sort((a, b) => b[2] - a[2]);
       } else {
-        const desempat = data.sort((a, b) => b[2] - a[2])
-        return desempat.sort((a, b) => b[1].length - a[1].length)
+        const desempat = data.sort((a, b) => b[2] - a[2]);
+        return desempat.sort((a, b) => b[1].length - a[1].length);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
 html {
-  font-family: 'Cabin', sans-serif;
+  font-family: "Cabin", sans-serif;
 }
 
 .page-container {
@@ -199,23 +225,23 @@ html {
     align-items: flex-start;
   }
   .selector {
-    margin-bottom: .4rem;
+    margin-bottom: 0.4rem;
   }
 }
 
 .summary {
   text-align: center;
-  margin-top: .6rem;
+  margin-top: 0.6rem;
   color: #444;
 }
 
 .cards-container {
-/*   display: flex;
+  /*   display: flex;
   flex-wrap: wrap; */
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   margin: 1rem auto 0;
-  gap: .6rem;
+  gap: 0.6rem;
 }
 
 @media (max-width: 1590px) {
@@ -238,7 +264,7 @@ html {
 
 .flip-cards-enter-active,
 .flip-cards-leave-active {
-  transition: all .7s;
+  transition: all 0.7s;
 }
 
 .flip-cards-enter,
@@ -247,15 +273,15 @@ html {
 }
 
 .flip-cards-move {
-  transition: transform .7s;
+  transition: transform 0.7s;
 }
 
 .link-sp {
   text-decoration: none;
-  font-size: .9rem;
+  font-size: 0.9rem;
   background-color: #148514b3;
   color: white;
-  padding: .25rem;
+  padding: 0.25rem;
   border: 1px solid green;
   border-radius: 3px;
 }
